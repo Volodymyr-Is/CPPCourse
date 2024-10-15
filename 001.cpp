ffe9a8866cbf4253e8f2e4cb5a04a90d79c1ac7f
Volodymyr-Is <144656592+Volodymyr-Is@users.noreply.github.com>
2024-10-15 18:52:08 +0300

    Lab2

---
+++ 001.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+int countElements(int**& arr, int rows, int cols) {
+    int min = 1, max = 0, count = 0;
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+        {
+            if (arr[i][j] < min)
+                min = arr[i][j];
+            if (arr[i][j] > max)
+                max = arr[i][j];
+        }
+
+    double res = (max / min);
+
+    for (int i = 0; i < rows; i+=2)
+        for (int j = 0; j < cols; j++)
+        {
+            if (arr[i][j] > res)
+                count++;
+        }
+
+    return count;
+}
+
+
+int main()
+{
+    /*int a = 10;
+    cout << "Hello World!\n";
+
+    cout << a << endl;*/
+
+
+
+    //char a;
+    //cin >> a;
+    ////cin.get(a);
+    //cout << (int)a << endl;
+    //cin.get();
+
+
+
+    //cout << 1. / 2 << endl; // 1. == 1.0 == double
+
+
+    int n = 5;
+    int** arr = new int* [n];
+
+    for (int i = 0; i < n; i++)
+        arr[i] = new int[n];
+
+    int randNum;
+    for (int i = 0; i < n; i++, cout << endl)
+        for (int j = 0; j < n; j++)
+        {
+            //randNum = rand() % 20 - 10;
+            //if(randNum != 0)
+            //    arr[i][j] = randNum;
+            arr[i][j] = rand() % 21 - 10;
+            //else
+            //    arr[i][j] = 1;
+            cout << arr[i][j] << "\t";
+        }
+    cout << endl;
+
+    int result = countElements(arr, n, n);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
